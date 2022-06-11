30793cf96edf36fe50b8a92f754ce06cc536ca65
Andy Chen <chenyangpoppin@hotmail.com>
2022-06-11 22:36:22 +0800

    feat: adding a simple example file

---
+++ src/example2.ts
@@ -0,0 +1,13 @@
+export let someStringVar: string = String(9);
+
+export type SumArgs = {
+    firstArgument: number;
+    secondArgument: number;
+};
+
+export function sum({firstArgument, secondArgument}: SumArgs) {
+    return firstArgument + secondArgument;
+}
+
+let result = sum({ firstArgument: 1, secondArgument: 2 });
+console.log(result);
