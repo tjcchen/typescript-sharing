4482b6bbdc8fb64364d42e5d237214c39cd1e6f7
Andy Chen <chenyangpoppin@hotmail.com>
2022-06-27 23:41:34 +0800

    feat: adding tsx component

---
+++ src/reactapp/src/util/javascript.jsx
@@ -0,0 +1,12 @@
+export default function JavaScript() {
+  const user = {
+    name: 'John',
+    age: 55
+  };
+
+  return (
+    <>
+      <p>address: {user.address}</p>
+    </>
+  );
+}
---
+++ src/reactapp/src/util/typescript.tsx
@@ -0,0 +1,12 @@
+export default function TypeScript() {
+  const user = {
+    name: 'John',
+    age: 55,
+  };
+
+  return (
+    <>
+      <p>{user.name}</p>
+    </>
+  );
+}
