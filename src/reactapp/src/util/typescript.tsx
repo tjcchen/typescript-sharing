2fac997004c9a70f863e4d667a776c070cd793e9
Andy Chen <chenyangpoppin@hotmail.com>
2022-06-28 23:32:24 +0800

    feat: adding ts intros

--- src/reactapp/src/util/typescript.tsx
+++ src/reactapp/src/util/typescript.tsx
@@ -1,3 +1,14 @@
+/**
+ * TypeScript Intro
+ * 1. Language which builds on JavaScript
+ * 2. TypeScript code is transformed in JS through compiler. (eg: TS compiler, Babel)
+ * 3. Goal: writing Typed JavaScript
+ * 
+ * Two main pros:
+ * 1. Type checking errors => catch errors before they occur in production
+ * 2. Providing better documentation => makes it much easier for you and others to understand the code
+ */
+
 export default function TypeScript() {
   const user = {
     name: 'John',
