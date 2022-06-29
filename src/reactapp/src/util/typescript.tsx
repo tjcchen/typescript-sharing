0fe4a0ec38ffdfbcc2d3c74c74b774f8f335f412
Andy Chen <chenyangpoppin@hotmail.com>
2022-06-29 23:38:43 +0800

    docs: comment thing

--- src/reactapp/src/util/typescript.tsx
+++ src/reactapp/src/util/typescript.tsx
@@ -4,9 +4,11 @@
  * 2. TypeScript code is transformed in JS through compiler. (eg: TS compiler, Babel)
  * 3. Goal: writing Typed JavaScript
  * 
- * Two main pros:
+ * Several main pros:
  * 1. Type checking errors => catch errors before they occur in production
  * 2. Providing better documentation => makes it much easier for you and others to understand the code
+ * 3. More reliable and easier to understand codebase( benefits you during building new features, bugfixes and refactors )
+ * 4. You'll start appreciate it when your codebase grows large or when multiple developers work on it
  */
 
 export default function TypeScript() {
