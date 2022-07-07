3431d490c9cdcd026908650b62c81e4e15a20e3a
Andy Chen <chenyangpoppin@hotmail.com>
2022-07-07 23:28:57 +0800

    feat: adding logic check

--- src/reactapp/src/App4.tsx
+++ src/reactapp/src/App4.tsx
@@ -7,10 +7,35 @@
  * https://react-typescript-cheatsheet.netlify.app/docs/basic/troubleshooting/types/#enum-types
  */
 
+import { useState } from "react";
+
+// Details
+// Shipping
+// Payment
+
 export default function App4() {
+    const [checkoutStep, setCheckoutStep] = useState('Details');
+
+    // Logic check
     return (
         <>
-            <p>TypeScript Enums</p>
+            { checkoutStep === 'Details' &&
+                <>
+                    <h1>Details</h1>
+                    <button type="button" onClick={() => setCheckoutStep('Shipping')}>Next</button>
+                </>
+            }
+            { checkoutStep === 'Shipping' &&
+                <>
+                    <h1>Shipping</h1>
+                    <button type="button" onClick={() => setCheckoutStep('Payment')}>Next</button>
+                </>
+            }
+            { checkoutStep === 'Payment' &&
+                <>
+                    <h1>Payment</h1>
+                </>
+            }
         </>
     );
 }
