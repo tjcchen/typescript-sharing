30a63cd97ee139693a073d803853e2ec4b02f992
Andy Chen <chenyangpoppin@hotmail.com>
2022-07-08 23:58:45 +0800

    feat: adding data for generic componnet

--- src/reactapp/src/App5.tsx
+++ src/reactapp/src/App5.tsx
@@ -6,5 +6,15 @@
  */
 
 export default function App5() {
+    const users = [
+        { id: 1, name: 'John', age: 55 },
+        { id: 2, name: 'Mitchel', age: 23 },
+        { id: 3, name: 'Mike', age: 55 },
+    ];
 
+    return (
+        <>
+            <p>Generic Componnets</p>
+        </>
+    );
 }
