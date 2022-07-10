73f20484b57cc80de65eb95a31b543a623359b49
Andy Chen <chenyangpoppin@hotmail.com>
2022-07-10 17:50:46 +0800

    feat: adding equality narrowing

--- src/reactapp/src/App6.tsx
+++ src/reactapp/src/App6.tsx
@@ -13,11 +13,13 @@ export default function App5() {
     const items: Item[] = [
         {
             id: 1,
+            type: 'imageItem',
             title: 'A nice sunset',
             imageUrl: 'https://img.freepik.com/free-vector/rice-field-terraces-illustration_107791-5424.jpg?w=2000&t=st=1657443356~exp=1657443956~hmac=a04cf62b3d4d632186d2520ac643ea05ced0f13b543d8bee236cda4c23ff2c3c'
         },
         {
             id: 2,
+            type: 'quoteItem',
             quote: 'This is a short quote, please look at this part'
         }
     ];
--- src/reactapp/src/components/component.tsx
+++ src/reactapp/src/components/component.tsx
@@ -4,12 +4,14 @@
 
 interface ImageItem {
     id: number;
+    type: 'imageItem';
     title: string;
     imageUrl: string;
 }
 
 interface QuoteItem {
     id: number;
+    type: 'quoteItem';
     quote: string;
 }
 
@@ -20,12 +22,14 @@ interface ComponentProps {
 }
 
 export default function Component({ items }: ComponentProps) {
-    // [IMPORTANT] Use in operator to make reactjs to differentiate between ImageItem and QuoteItem
+    // [IMPORTANT]
+    // 1. Use in operator to make reactjs to differentiate between ImageItem and QuoteItem
+    // 2. Or to use quality narrowing ( based on a type property )
     return (
         <ul>
             {
                 items.map((item: Item) => {
-                    if ('title' in item) { // ImageItem
+                    if (item.type === 'imageItem') { // ImageItem. eg: 'title' in item
                         return (
                             <li key={ item.id }>
                                 { item.title && <p>{ item.title }</p> }
