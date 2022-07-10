a752eccf6ecd43a9550f32163d6194208e825ad6
Andy Chen <chenyangpoppin@hotmail.com>
2022-07-10 17:35:41 +0800

    feat: refator code with with union type

--- src/reactapp/src/components/component.tsx
+++ src/reactapp/src/components/component.tsx
@@ -2,38 +2,69 @@
  * A type narrowing component
  */
 
-export interface Item {
+interface ImageItem {
     id: number;
-    title?: string;
-    imageUrl?: string;
-    quote?: string;
+    title: string;
+    imageUrl: string;
 }
+
+interface QuoteItem {
+    id: number;
+    quote: string;
+}
+
+export type Item = ImageItem | QuoteItem;
 
 interface ComponentProps {
     items: Item[];
 }
 
 export default function Component({ items }: ComponentProps) {
+    // [IMPORTANT] Use in operator to make reactjs to differentiate between ImageItem and QuoteItem
     return (
         <ul>
             {
                 items.map((item: Item) => {
-                    return (
-                        <li key={ item.id }>
-                            { item.title && <p>{ item.title }</p> }
-                            {
+                    if ('title' in item) { // ImageItem
+                        return (
+                            <li key={ item.id }>
+                                { item.title && <p>{ item.title }</p> }
+                                {
 
-                                item.imageUrl && (
-                                    <img
-                                        style={{ maxWidth: '15rem' }}
-                                        alt={ item.title }
-                                        src={ item.imageUrl }
-                                    />
-                                )
-                            }
-                            { item.quote && <p style={{ fontStyle: 'italic' }}>{ item.quote }</p> }
-                        </li>
-                    );
+                                    item.imageUrl && (
+                                        <img
+                                            style={{ maxWidth: '15rem' }}
+                                            alt={ item.title }
+                                            src={ item.imageUrl }
+                                        />
+                                    )
+                                }
+                            </li>
+                        );
+                    } else { // QuoteItem
+                        return (
+                            <li key={ item.id }>
+                                { item.quote && <p style={{ fontStyle: 'italic' }}>{ item.quote }</p> }
+                            </li>
+                        );
+                    }
+
+                    // return (
+                    //     <li key={ item.id }>
+                    //         { item.title && <p>{ item.title }</p> }
+                    //         {
+
+                    //             item.imageUrl && (
+                    //                 <img
+                    //                     style={{ maxWidth: '15rem' }}
+                    //                     alt={ item.title }
+                    //                     src={ item.imageUrl }
+                    //                 />
+                    //             )
+                    //         }
+                    //         { item.quote && <p style={{ fontStyle: 'italic' }}>{ item.quote }</p> }
+                    //     </li>
+                    // );
                 })
             }
         </ul>
